f839474b30de89d8ab101320545dfb64ed872353
Shruti Sharma <dcsa2325shrutisharma@kuk.ac.in>
2025-07-22 15:23:35 +0530

    404 fix

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io(`${import.meta.env.VITE_API_URL}`);
+export const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ["websocket"], // Avoid polling (Render hates it)
+  withCredentials: true
+});
 
-// Call this after user login
 export const registerSocket = (userId) => {
   socket.emit("register", userId);
 };
